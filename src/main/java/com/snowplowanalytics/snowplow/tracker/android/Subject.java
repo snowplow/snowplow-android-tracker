82ec7d23e96f3a9cd17fb6edbc7dbcc2a33956c9
Jonathan Almeida <jonathan@snowplowanalytics.com>
2014-07-29 20:07:26 -0400

    Added new Subject class

---
+++ src/main/java/com/snowplowanalytics/snowplow/tracker/android/Subject.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+package com.snowplowanalytics.snowplow.tracker.android;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.WindowManager;
+
+import com.snowplowanalytics.snowplow.tracker.DevicePlatform;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class Subject extends com.snowplowanalytics.snowplow.tracker.Subject {
+
+    public Subject() {
+        // Default Platform
+        this.setPlatform(DevicePlatform.Mobile);
+
+        // Default Timezone
+        setDefaultTimezone();
+    }
+
+    public Subject(Context context) {
+        // Default Platform
+        this.setPlatform(DevicePlatform.Mobile);
+
+        // Default Timezone
+        setDefaultTimezone();
+
+        // Default Screen Resolution
+        setDefaultScreenResolution(context);
+
+        // Default Language
+        setDefaultLanguage();
+    }
+
+    private void setDefaultScreenResolution(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        this.setScreenResolution(size.x, size.y);
+    }
+
+    private void setDefaultTimezone() {
+        TimeZone tz = Calendar.getInstance().getTimeZone();
+        this.setTimezone(tz.getID());
+    }
+
+    private void setDefaultLanguage() {
+        this.setLanguage(Locale.getDefault().getDisplayLanguage());
+    }
+}
