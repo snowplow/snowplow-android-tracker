eadeef0d64c45c8b3d6a5d769bb698382272b750
Josh <joshua.a.beemster@gmail.com>
2015-02-03 19:10:23 +0100

    Updated emitter test to represent changes to the Tracker and Emitter

--- snowplow-android-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/EmitterTest.java
+++ snowplow-android-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/EmitterTest.java
@@ -1,44 +1,48 @@
 package com.snowplowanalytics.snowplow.tracker;
 
-import com.snowplowanalytics.snowplow.tracker.emitter_utils.*;
+import com.snowplowanalytics.snowplow.tracker.utils.emitter.BufferOption;
+import com.snowplowanalytics.snowplow.tracker.utils.emitter.HttpMethod;
 
 import android.test.AndroidTestCase;
 
 public class EmitterTest extends AndroidTestCase {
 
-    private static String testURL = "10.0.2.2:4545";
-    //private static String testURL = "87f093a.ngrok.com";
-
-    public void testSendGetData() throws Exception {
-        Emitter emitter = new Emitter
-                .EmitterBuilder(testURL, getContext())
-                .httpMethod(HttpMethod.GET)
-                .build();
-        Subject subject = new Subject(getContext());
-        emitter.setBufferOption(BufferOption.Instant);
-        Tracker tracker = new Tracker
-                .TrackerBuilder(emitter, "myNamespace", "myAppId")
-                .base64(false)
-                .subject(subject)
-                .build();
-        tracker.trackScreenView("Screen 1", null);
-    }
+    //private static String testURL = "10.0.2.2:4545";
+    private static String testURL = "77278b85.ngrok.com";
 
     public void testSendPostData() throws Exception {
         Emitter emitter = new Emitter
-                .EmitterBuilder(testURL, getContext())
+                .EmitterBuilder(testURL)
                 .httpMethod(HttpMethod.POST)
                 .build();
         Subject subject = new Subject(getContext());
         emitter.setBufferOption(BufferOption.Instant);
-        Tracker tracker = new Tracker
-                .TrackerBuilder(emitter, "myNamespace", "myAppId")
+
+        new Tracker
+                .TrackerBuilder(emitter, "myNamespace", "myAppId", getContext())
                 .base64(false)
                 .subject(subject)
                 .build();
-        tracker.trackScreenView("Screen 1", null);
+        Tracker.getInstance().trackScreenView("Screen 1", null);
 
-        // Sleep added to allow for background processes to complete
-        Thread.sleep(2000);
+        Thread.sleep(10000);
+    }
+
+    public void testSendGetData() throws Exception {
+        Emitter emitter = new Emitter
+                .EmitterBuilder(testURL)
+                .httpMethod(HttpMethod.GET)
+                .build();
+        Subject subject = new Subject(getContext());
+        emitter.setBufferOption(BufferOption.Instant);
+
+        new Tracker
+                .TrackerBuilder(emitter, "myNamespace", "myAppId", getContext())
+                .base64(false)
+                .subject(subject)
+                .build();
+        Tracker.getInstance().trackScreenView("Screen 1", null);
+
+        Thread.sleep(10000);
     }
 }
