18b17fb2bbbf7ce4c622abfed77945e7be657dcd
Josh <joshua.a.beemster@gmail.com>
2015-02-03 19:06:45 +0100

    Added Helper classes for Emitting events

---
+++ snowplow-android-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/utils/emitter/RequestResult.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+package com.snowplowanalytics.snowplow.tracker.utils.emitter;
+
+import java.util.LinkedList;
+
+/**
+ * Stores the result of a Request Attempt
+ */
+public class RequestResult {
+
+    private final boolean success;
+    private final LinkedList<Long> eventIds;
+
+    public RequestResult(boolean success, LinkedList<Long> eventIds) {
+        this.success = success;
+        this.eventIds = eventIds;
+    }
+
+    public boolean getSuccess() {
+        return this.success;
+    }
+
+    public LinkedList<Long> getEventIds() {
+        return this.eventIds;
+    }
+}
