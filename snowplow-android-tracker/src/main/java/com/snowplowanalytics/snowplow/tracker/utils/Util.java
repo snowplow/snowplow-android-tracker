b5d644ec01b0de57cfdd8f9bff1225d7130dad0a
Josh <joshua.a.beemster@gmail.com>
2015-02-05 20:27:50 +0100

    Removed extraneous functions from Util

--- snowplow-android-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/utils/Util.java
+++ snowplow-android-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/utils/Util.java
@@ -13,124 +13,50 @@
 
 package com.snowplowanalytics.snowplow.tracker.utils;
 
-import android.content.Context;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.util.Base64;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import java.util.UUID;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
-import java.lang.reflect.Method;
-
+/**
+ * Provides basic Utilities for the
+ * Snowplow Tracker.
+ */
 public class Util {
 
     private static ObjectMapper sObjectMapper = new ObjectMapper();
-
-    public static String getCarrier(Context context) {
-        String carrierName = "";
-        TelephonyManager telephonyManager =
-                (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        if (telephonyManager != null) {
-            carrierName = telephonyManager.getNetworkOperatorName();
-        }
-        return carrierName;
-    }
-
-    public static Location getLocation(Context context) {
-        Location location = null;
-        LocationManager locationManager =
-                (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        criteria.setPowerRequirement(Criteria.POWER_LOW);
-        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-
-        String provider = locationManager.getBestProvider(criteria, true);
-        if (provider != null) {
-            try {
-                location = locationManager.getLastKnownLocation(provider);
-            } catch (SecurityException ex) {
-                Log.d(Util.class.toString(), "No permission to retrieve location.");
-                location = null;
-            }
-        }
-        return location;
-    }
-
-    private static Object invokeStaticMethod(String className, String methodName,
-                                             Class[] cArgs, Object... args)
-            throws Exception {
-        Class classObject = Class.forName(className);
-
-        return invokeMethod(classObject, methodName, null, cArgs, args);
-    }
-
-    private static Object invokeInstanceMethod(Object instance, String methodName,
-                                               Class[] cArgs, Object... args)
-            throws Exception {
-        Class classObject = instance.getClass();
-
-        return invokeMethod(classObject, methodName, instance, cArgs, args);
-    }
-
-    private static Object invokeMethod(Class classObject, String methodName, Object instance,
-                                       Class[] cArgs, Object... args)
-            throws Exception {
-        Method methodObject = classObject.getMethod(methodName, cArgs);
-
-        Object resultObject = methodObject.invoke(instance, args);
-
-        return resultObject;
-    }
 
     public static ObjectMapper defaultMapper() {
         return sObjectMapper;
     }
 
-    @Deprecated
-    public static JsonNode stringToJsonNode(String str) {
-        try {
-            return defaultMapper().readTree(str);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static JsonNode mapToJsonNode(Map map) {
-        return defaultMapper().valueToTree(map);
-    }
-
-    public static int getTransactionId() {
-        Random r = new Random();
-        return r.nextInt(999999-100000+1) + 100000;
-    }
-
+    /**
+     * Returns the current System time
+     * as a String.
+     *
+     * @return the system time as a string
+     */
     public static String getTimestamp() {
         return Long.toString(System.currentTimeMillis());
     }
 
-    /** 
-     * Addition functions
-     * Used to add different sources of key=>value pairs to a map.
-     * Map is then used to build "Associative array for getter function.
-     * Some use Base64 encoding
+    /**
+     * Encodes a string into Base64.
+     *
+     * @param string the string too encode
+     * @return a Base64 encoded string
      */
-
     public static String base64Encode(String string) {
         return Base64.encodeToString(string.getBytes(), Base64.URL_SAFE);
     }
 
+    /**
+     * Generates a random UUID for
+     * each event.
+     *
+     * @return a UUID string
+     */
     public static String getEventId() {
         return UUID.randomUUID().toString();
     }
