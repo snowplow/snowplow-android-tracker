1050aaeb1f4da0b1508f06e54d8003376f1421ae
Josh <joshua.a.beemster@gmail.com>
2015-02-05 20:29:48 +0100

    Updated tests to represent code changes

--- snowplow-android-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/EmitterTest.java
+++ snowplow-android-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/EmitterTest.java
@@ -7,41 +7,27 @@ import android.test.AndroidTestCase;
 
 public class EmitterTest extends AndroidTestCase {
 
-    //private static String testURL = "10.0.2.2:4545";
-    private static String testURL = "77278b85.ngrok.com";
-
-    public void testSendPostData() throws Exception {
-        Emitter emitter = new Emitter
-                .EmitterBuilder(testURL)
-                .httpMethod(HttpMethod.POST)
-                .build();
-        Subject subject = new Subject(getContext());
-        emitter.setBufferOption(BufferOption.Instant);
-
-        new Tracker
-                .TrackerBuilder(emitter, "myNamespace", "myAppId", getContext())
-                .base64(false)
-                .subject(subject)
-                .build();
-        Tracker.getInstance().trackScreenView("Screen 1", null);
-
-        Thread.sleep(10000);
-    }
+    private static String testURL = "10.0.2.2:4545";
+    //private static String testURL = "77278b85.ngrok.com";
 
     public void testSendGetData() throws Exception {
         Emitter emitter = new Emitter
-                .EmitterBuilder(testURL)
-                .httpMethod(HttpMethod.GET)
+                .EmitterBuilder(testURL, getContext())
+                .httpMethod(HttpMethod.POST)
                 .build();
-        Subject subject = new Subject(getContext());
-        emitter.setBufferOption(BufferOption.Instant);
 
-        new Tracker
-                .TrackerBuilder(emitter, "myNamespace", "myAppId", getContext())
+        Subject subject = new Subject(getContext());
+
+        emitter.setBufferOption(BufferOption.Default);
+
+        Tracker tracker = new Tracker
+                .TrackerBuilder(emitter, "myNamespace", "myAppId")
                 .base64(false)
                 .subject(subject)
                 .build();
-        Tracker.getInstance().trackScreenView("Screen 1", null);
+
+        for (int i = 0; i < 10; i++)
+            tracker.trackScreenView("Screen 1", null);
 
         Thread.sleep(10000);
     }
