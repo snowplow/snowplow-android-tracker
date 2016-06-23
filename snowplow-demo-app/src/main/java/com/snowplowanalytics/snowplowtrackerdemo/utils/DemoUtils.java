a43c68c662e180043ebbec43cf10f594a6b4679b
Josh <joshua.a.beemster@gmail.com>
2016-06-23 15:39:46 +0200

    Demo App Updates

--- snowplow-demo-app/src/main/java/com/snowplowanalytics/snowplowtrackerdemo/utils/DemoUtils.java
+++ snowplow-demo-app/src/main/java/com/snowplowanalytics/snowplowtrackerdemo/utils/DemoUtils.java
@@ -13,15 +13,6 @@
 
 package com.snowplowanalytics.snowplowtrackerdemo.utils;
 
-import com.snowplowanalytics.snowplow.tracker.DevicePlatforms;
-import com.snowplowanalytics.snowplow.tracker.utils.LogLevel;
-import com.snowplowanalytics.snowplow.tracker.emitter.RequestCallback;
-import com.snowplowanalytics.snowplow.tracker.Tracker;
-import com.snowplowanalytics.snowplow.tracker.Subject;
-import com.snowplowanalytics.snowplow.tracker.Emitter;
-
-import android.content.Context;
-
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -32,81 +23,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class DemoUtils {
 
-    private static final String namespace = "SnowplowAndroidTrackerDemo";
-    private static final String appId = "DemoID";
-
-    // Tracker Utils
-
-    /**
-     * Returns a Classic Tracker
-     *
-     * @param context the application context
-     * @param callback the emitter callback
-     * @return a new Classic Tracker
-     */
-    public static Tracker getAndroidTracker(Context context, RequestCallback callback) {
-        Emitter emitter = DemoUtils.getEmitter(context, callback);
-        Subject subject = DemoUtils.getSubject(context);
-        return DemoUtils.getTracker(emitter, subject, context);
-    }
-
-    /**
-     * Returns a Classic Tracker
-     *
-     * @param emitter a Classic emitter
-     * @param subject the tracker subject
-     * @return a new Classic Tracker
-     */
-    private static Tracker getTracker(Emitter emitter, Subject subject, Context context) {
-        return new Tracker.TrackerBuilder(emitter, namespace, appId, context)
-                .level(LogLevel.DEBUG)
-                .base64(false)
-                .platform(DevicePlatforms.Mobile)
-                .subject(subject)
-                .threadCount(20)
-                .sessionContext(true)
-                .build();
-    }
-
-    /**
-     * Returns a Classic Emitter
-     *
-     * @param context the application context
-     * @param callback the emitter callback
-     * @return a new Classic Emitter
-     */
-    private static Emitter getEmitter(Context context, RequestCallback callback) {
-        return new Emitter.EmitterBuilder("", context)
-                .callback(callback)
-                .tick(1)
-                .build();
-    }
-
-    /**
-     * Returns a Subject Object
-     *
-     * @param context the application context
-     * @return a new subject
-     */
-    private static Subject getSubject(Context context) {
-        return new Subject
-                .SubjectBuilder()
-                .context(context)
-                .build();
-    }
-
-    // Executor Utils
-
     private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-
-    /**
-     * Executes a runnable on the executor service
-     *
-     * @param runnable a new task
-     */
-    public static void execute(Runnable runnable) {
-        executor.execute(runnable);
-    }
 
     /**
      * Executes a repeating runnable
