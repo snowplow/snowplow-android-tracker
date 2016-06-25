5992103d8c3c5e1a34e244b343c5707272fdeadb
Josh <joshua.a.beemster@gmail.com>
2016-06-25 17:05:59 +0200

    Automatically track fatal/uncaught application exceptions (closes #202)

--- snowplow-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/constants/Parameters.java
+++ snowplow-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/constants/Parameters.java
@@ -119,4 +119,15 @@ public class Parameters {
     public static final String SESSION_INDEX = "sessionIndex";
     public static final String SESSION_STORAGE = "storageMechanism";
     public static final String SESSION_FIRST_ID = "firstEventId";
+
+    // Application Crash
+    public static final String APP_ERROR_MESSAGE = "message";
+    public static final String APP_ERROR_STACK = "stackTrace";
+    public static final String APP_ERROR_THREAD_NAME = "threadName";
+    public static final String APP_ERROR_THREAD_ID = "threadId";
+    public static final String APP_ERROR_LANG = "programmingLanguage";
+    public static final String APP_ERROR_LINE = "lineNumber";
+    public static final String APP_ERROR_CLASS_NAME = "className";
+    public static final String APP_ERROR_EXCEPTION_NAME = "exceptionName";
+    public static final String APP_ERROR_FATAL = "isFatal";
 }
