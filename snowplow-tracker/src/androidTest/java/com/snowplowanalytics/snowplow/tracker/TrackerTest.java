5b50319b5a86522c15b99bbdd688244786d30324
mhadam <michael@hadam.us>
2018-10-11 17:16:53 -0400

    Bump to version 0.9.0-M4

--- snowplow-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/TrackerTest.java
+++ snowplow-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/TrackerTest.java
@@ -92,7 +92,7 @@ public class TrackerTest extends AndroidTestCase {
         assertEquals(false, tracker.getBase64Encoded());
         assertNotNull(tracker.getEmitter());
         assertNotNull(tracker.getSubject());
-        assertEquals("andr-0.9.0-M3", tracker.getTrackerVersion());
+        assertEquals("andr-0.9.0-M4", tracker.getTrackerVersion());
         assertEquals(LogLevel.VERBOSE, tracker.getLogLevel());
         assertEquals(2, tracker.getThreadCount());
         assertEquals(false, tracker.getApplicationCrash());
