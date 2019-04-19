5c52820c3aef3d687d921e606a83de1597a7063a
Oguzhan Unlu <cengoguzhanunlu@gmail.com>
2019-04-19 03:48:59 +0300

    Prepared for release

--- snowplow-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/TrackerTest.java
+++ snowplow-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/TrackerTest.java
@@ -94,7 +94,7 @@ public class TrackerTest extends AndroidTestCase {
         assertEquals(false, tracker.getBase64Encoded());
         assertNotNull(tracker.getEmitter());
         assertNotNull(tracker.getSubject());
-        assertEquals("andr-1.0.1", tracker.getTrackerVersion());
+        assertEquals("andr-1.1.0", tracker.getTrackerVersion());
         assertEquals(LogLevel.VERBOSE, tracker.getLogLevel());
         assertEquals(2, tracker.getThreadCount());
         assertEquals(false, tracker.getApplicationCrash());
