1201c0b8115cb80fa5accd0e6edfe191083c3a1d
Alex Benini <alex.benini@snowplowanalytics.com>
2020-06-05 12:18:23 +0200

    Add copyright comment

--- snowplow-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/TrackerEvent.java
+++ snowplow-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/TrackerEvent.java
@@ -1,11 +1,21 @@
-package com.snowplowanalytics.snowplow.tracker;
+/*
+ * Copyright (c) 2015-2020 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
 
-import android.support.annotation.NonNull;
+package com.snowplowanalytics.snowplow.tracker;
 
 import com.snowplowanalytics.snowplow.tracker.events.AbstractPrimitive;
 import com.snowplowanalytics.snowplow.tracker.events.AbstractSelfDescribing;
 import com.snowplowanalytics.snowplow.tracker.events.Event;
-import com.snowplowanalytics.snowplow.tracker.events.SelfDescribing;
 import com.snowplowanalytics.snowplow.tracker.events.TrackerError;
 import com.snowplowanalytics.snowplow.tracker.payload.SelfDescribingJson;
 
