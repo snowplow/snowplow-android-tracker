81c4b049355e144be28fbbca03ef7577f0711e28
matus-tomlein <matus-tomlein@users.noreply.github.com>
2022-03-09 11:08:43 +0000

    deploy: 50e104b49748fb02118fc5d6c71e8fffc39969f3

--- search/functions_a.js
+++ search/functions_a.js
@@ -6,6 +6,6 @@ var searchData=
   ['lifecycleentity_3',['LifecycleEntity',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1entity_1_1_lifecycle_entity.html#a203564701956ae170d1d4fff196c3753',1,'com::snowplowanalytics::snowplow::entity::LifecycleEntity']]],
   ['lifecyclestate_4',['LifecycleState',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1internal_1_1tracker_1_1_lifecycle_state.html#ad94531798c28eaff7000e373efa1ee51',1,'com::snowplowanalytics::snowplow::internal::tracker::LifecycleState']]],
   ['loggerdelegate_5',['loggerDelegate',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1configuration_1_1_tracker_configuration.html#a20660014d4fdf1a349baa9562d5c650c',1,'com::snowplowanalytics::snowplow::configuration::TrackerConfiguration']]],
-  ['loglevel_6',['logLevel',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1configuration_1_1_tracker_configuration.html#abd22783650fd13e88e1b3c1e1378723c',1,'com::snowplowanalytics::snowplow::configuration::TrackerConfiguration']]],
-  ['loglevel_7',['LogLevel',['../enumcom_1_1snowplowanalytics_1_1snowplow_1_1tracker_1_1_log_level.html#a451e3c8677fd194146dcc3a6a95931bd',1,'com::snowplowanalytics::snowplow::tracker::LogLevel']]]
+  ['loglevel_6',['LogLevel',['../enumcom_1_1snowplowanalytics_1_1snowplow_1_1tracker_1_1_log_level.html#a451e3c8677fd194146dcc3a6a95931bd',1,'com::snowplowanalytics::snowplow::tracker::LogLevel']]],
+  ['loglevel_7',['logLevel',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1configuration_1_1_tracker_configuration.html#abd22783650fd13e88e1b3c1e1378723c',1,'com::snowplowanalytics::snowplow::configuration::TrackerConfiguration']]]
 ];
--- search/variables_b.js
+++ search/variables_b.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['name_0',['name',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1event_1_1_ecommerce_transaction_item.html#a3fa1f79fc48ac916abbaf1fd38de952e',1,'com.snowplowanalytics.snowplow.event.EcommerceTransactionItem.name()'],['../classcom_1_1snowplowanalytics_1_1snowplow_1_1event_1_1_screen_view.html#a2df36aba411b52115ad0863b0f64a1e3',1,'com.snowplowanalytics.snowplow.event.ScreenView.name()']]],
-  ['namespace_1',['NAMESPACE',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1internal_1_1constants_1_1_parameters.html#a6b6253cc87cd6806c53ed27fb2fbb163',1,'com::snowplowanalytics::snowplow::internal::constants::Parameters']]],
-  ['namespace_2',['namespace',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1internal_1_1remoteconfiguration_1_1_configuration_bundle.html#a7b36b4080c381560566130cf2af974b9',1,'com::snowplowanalytics::snowplow::internal::remoteconfiguration::ConfigurationBundle']]],
+  ['namespace_1',['namespace',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1internal_1_1remoteconfiguration_1_1_configuration_bundle.html#a7b36b4080c381560566130cf2af974b9',1,'com::snowplowanalytics::snowplow::internal::remoteconfiguration::ConfigurationBundle']]],
+  ['namespace_2',['NAMESPACE',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1internal_1_1constants_1_1_parameters.html#a6b6253cc87cd6806c53ed27fb2fbb163',1,'com::snowplowanalytics::snowplow::internal::constants::Parameters']]],
   ['network_5ftechnology_3',['NETWORK_TECHNOLOGY',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1internal_1_1constants_1_1_parameters.html#ad44df34f7b0a0840384681c05ef03e1e',1,'com::snowplowanalytics::snowplow::internal::constants::Parameters']]],
   ['network_5ftype_4',['NETWORK_TYPE',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1internal_1_1constants_1_1_parameters.html#a692ef5f71baafbb2ffcfd017e2dad1e1',1,'com::snowplowanalytics::snowplow::internal::constants::Parameters']]],
   ['network_5fuid_5',['NETWORK_UID',['../classcom_1_1snowplowanalytics_1_1snowplow_1_1internal_1_1constants_1_1_parameters.html#a8912a6d276a4924c18aeaa9975109216',1,'com::snowplowanalytics::snowplow::internal::constants::Parameters']]],
