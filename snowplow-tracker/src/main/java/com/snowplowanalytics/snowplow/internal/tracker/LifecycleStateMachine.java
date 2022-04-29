6cbeba0f096e037e555f6f3c08d01ef6cc3b641c
Miranda Wilson <miranda@snowplowanalytics.com>
2022-04-29 15:33:05 +0100

    Add tests

--- snowplow-tracker/src/main/java/com/snowplowanalytics/snowplow/internal/tracker/LifecycleStateMachine.java
+++ snowplow-tracker/src/main/java/com/snowplowanalytics/snowplow/internal/tracker/LifecycleStateMachine.java
@@ -17,6 +17,16 @@ import java.util.Map;
 
 public class LifecycleStateMachine implements StateMachineInterface {
 
+    /*
+     States: Visible, NotVisible
+     Events: FG (Foreground), BG (Background)
+     Transitions:
+      - Visible (BG) NotVisible
+      - NotVisible (FG) Visible
+     Entity Generation:
+      - Visible, NotVisible
+     */
+
     @NonNull
     @Override
     public List<String> subscribedEventSchemasForTransitions() {
