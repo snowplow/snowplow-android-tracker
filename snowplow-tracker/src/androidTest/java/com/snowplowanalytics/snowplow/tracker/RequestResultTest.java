28b63571b40e166ef66424a53f9a04ebb18eccf0
Matúš Tomlein <matus.tomlein@gmail.com>
2022-06-28 09:38:47 +0200

    Change RequestResultTest to AndroidJUnit4

--- snowplow-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/RequestResultTest.java
+++ snowplow-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/RequestResultTest.java
@@ -1,16 +1,25 @@
 package com.snowplowanalytics.snowplow.tracker;
 
-import android.test.AndroidTestCase;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.snowplowanalytics.snowplow.network.RequestResult;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RequestResultTest extends AndroidTestCase {
+@RunWith(AndroidJUnit4.class)
+public class RequestResultTest {
 
+    @Test
     public void testSuccessfulRequest() {
         RequestResult result = new RequestResult(200, false, Arrays.asList(100L));
         assertTrue(result.isSuccessful());
@@ -19,24 +28,28 @@ public class RequestResultTest extends AndroidTestCase {
         assertEquals(result.getEventIds(), Arrays.asList(100L));
     }
 
+    @Test
     public void testFailedRequest() {
         RequestResult result = new RequestResult(500, false, new ArrayList<>());
         assertFalse(result.isSuccessful());
         assertTrue(result.shouldRetry(new HashMap<>()));
     }
 
+    @Test
     public void testOversizedFailedRequest() {
         RequestResult result = new RequestResult(500, true, new ArrayList<>());
         assertFalse(result.isSuccessful());
         assertFalse(result.shouldRetry(new HashMap<>()));
     }
 
+    @Test
     public void testFailedRequestWithNoRetryStatus() {
         RequestResult result = new RequestResult(403, false, new ArrayList<>());
         assertFalse(result.isSuccessful());
         assertFalse(result.shouldRetry(new HashMap<>()));
     }
 
+    @Test
     public void testFailedRequestWithCustomRetryRules() {
         Map<Integer, Boolean> customRetry = new HashMap<>();
         customRetry.put(403, true);
