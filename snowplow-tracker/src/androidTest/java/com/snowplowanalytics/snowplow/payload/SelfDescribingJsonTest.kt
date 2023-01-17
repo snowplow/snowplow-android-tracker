9c6342f7b3a9cc51305a3938a18cba1fe2ce9d8d
Miranda Wilson <miranda@snowplowanalytics.com>
2023-01-17 17:11:28 +0000

    Translate payload tests

---
+++ snowplow-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/payload/SelfDescribingJsonTest.kt
@@ -0,0 +1,260 @@
+/*
+ * Copyright (c) 2015-2022 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.payload
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.json.JSONException
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SelfDescribingJsonTest {
+    private val testSchema = "org.test.scheme"
+    private var testMap: HashMap<String, Any?>? = null
+    private var testList: MutableList<Any?>? = null
+    @Before
+    fun setUp() {
+        testMap = HashMap()
+        testList = ArrayList()
+    }
+
+    @Test
+    fun testFailures() {
+        var exception = false
+        try {
+            SelfDescribingJson("")
+        } catch (e: Exception) {
+            Assert.assertEquals("schema cannot be empty", e.message)
+            exception = true
+        }
+        Assert.assertTrue(exception)
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithSchemaOnly() {
+        val json = SelfDescribingJson(testSchema)
+
+        // {"schema":"org.test.scheme","data":{}}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val d = map.getJSONObject("data")
+        Assert.assertEquals(0, d.length().toLong())
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithOurEmptyMap() {
+        val json = SelfDescribingJson(testSchema, testMap!!)
+
+        // {"schema":"org.test.scheme","data":{}}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val d = map.getJSONObject("data")
+        Assert.assertEquals(0, d.length().toLong())
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithSimpleMap() {
+        testMap!!["alpha"] = "beta"
+        val json = SelfDescribingJson(testSchema, testMap!!)
+
+        // {"schema":"org.test.scheme","data":{"alpha":"beta"}}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val d = map.getJSONObject("data")
+        Assert.assertEquals(1, d.length().toLong())
+        Assert.assertEquals("beta", d.getString("alpha"))
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithEmptyList() {
+        val json = SelfDescribingJson(testSchema, testList!!)
+
+        // {"schema":"org.test.scheme","data":[]}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val d = map.getJSONArray("data")
+        Assert.assertEquals(0, d.length().toLong())
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithSimpleList() {
+        testList!!.add("delta")
+        val json = SelfDescribingJson(testSchema, testList!!)
+
+        // {"schema":"org.test.scheme","data":["delta"]}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val d = map.getJSONArray("data")
+        Assert.assertEquals(1, d.length().toLong())
+        Assert.assertEquals("delta", d[0])
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithNestedList() {
+        val testInnerList: MutableList<String> = ArrayList()
+        testInnerList.add("gamma")
+        testInnerList.add("epsilon")
+        testList!!.add(testInnerList)
+        val json = SelfDescribingJson(testSchema, testList!!)
+
+        // {"schema":"org.test.scheme","data":[["gamma","epsilon"]]}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val list = map.getJSONArray("data")
+        Assert.assertEquals(1, list.length().toLong())
+        val innerList = list.getJSONArray(0)
+        Assert.assertEquals(2, innerList.length().toLong())
+        Assert.assertEquals("gamma", innerList[0])
+        Assert.assertEquals("epsilon", innerList[1])
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithListOfMaps() {
+        testMap!!["a"] = "b"
+        testList!!.add(testMap)
+        testList!!.add(testMap)
+        val json = SelfDescribingJson(testSchema, testList!!)
+
+        // {"schema":"org.test.scheme","data":[{"a":"b"},{"a":"b"}]}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val list = map.getJSONArray("data")
+        Assert.assertEquals(2, list.length().toLong())
+        val innerListMap1 = list.getJSONObject(0)
+        Assert.assertEquals("b", innerListMap1.getString("a"))
+        val innerListMap2 = list.getJSONObject(0)
+        Assert.assertEquals("b", innerListMap2.getString("a"))
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithSelfDescribingJson() {
+        val json = SelfDescribingJson(testSchema, SelfDescribingJson(testSchema, testMap!!))
+
+        // {"schema":"org.test.scheme","data":{"schema":"org.test.scheme","data":{}}}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val innerMap = map.getJSONObject("data")
+        Assert.assertEquals(testSchema, innerMap.getString("schema"))
+        val innerData = innerMap.getJSONObject("data")
+        Assert.assertEquals(0, innerData.length().toLong())
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithSelfDescribingJsonWithMore() {
+        testMap!!["a"] = "b"
+        testMap!!["c"] = "d"
+        val json = SelfDescribingJson(testSchema, SelfDescribingJson(testSchema, testMap!!))
+
+        // {"schema":"org.test.scheme","data":{"schema":"org.test.scheme","data":{"a":"b","c":"d"}}}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val innerMap = map.getJSONObject("data")
+        Assert.assertEquals(testSchema, innerMap.getString("schema"))
+        val innerData = innerMap.getJSONObject("data")
+        Assert.assertEquals(2, innerData.length().toLong())
+        Assert.assertEquals("b", innerData.getString("a"))
+        Assert.assertEquals("d", innerData.getString("c"))
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateThenSetSelfDescribingJson() {
+        val json = SelfDescribingJson(testSchema)
+        json.setData(SelfDescribingJson(testSchema, testMap!!))
+
+        // {"schema":"org.test.scheme","data":{"schema":"org.test.scheme","data":{}}}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val innerMap = map.getJSONObject("data")
+        Assert.assertEquals(testSchema, innerMap.getString("schema"))
+        val innerData = innerMap.getJSONObject("data")
+        Assert.assertEquals(0, innerData.length().toLong())
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateWithTrackerPayload() {
+        val payload = TrackerPayload()
+        testMap!!["a"] = "b"
+        payload.addMap(testMap)
+        val json = SelfDescribingJson(testSchema, payload)
+
+        // {"schema":"org.test.scheme","data":{"a":"b"}}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val innerMap = map.getJSONObject("data")
+        Assert.assertEquals("b", innerMap.getString("a"))
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testCreateThenSetTrackerPayload() {
+        val payload = TrackerPayload()
+        testMap!!["a"] = "b"
+        payload.addMap(testMap)
+        val json = SelfDescribingJson(testSchema)
+        json.setData(payload)
+
+        // {"schema":"org.test.scheme","data":{"a":"b"}}
+        val s = json.toString()
+        val map = JSONObject(s)
+        Assert.assertEquals(testSchema, map.getString("schema"))
+        val innerMap = map.getJSONObject("data")
+        Assert.assertEquals("b", innerMap.getString("a"))
+    }
+
+    @Test
+    fun testSetNullValues() {
+        val json = SelfDescribingJson(testSchema)
+        Assert.assertEquals("{\"schema\":\"org.test.scheme\",\"data\":{}}", json.toString())
+        json.setData(null as TrackerPayload?)
+        Assert.assertEquals("{\"schema\":\"org.test.scheme\",\"data\":{}}", json.toString())
+        json.setData(null as Any?)
+        Assert.assertEquals("{\"schema\":\"org.test.scheme\",\"data\":{}}", json.toString())
+        json.setData(null as SelfDescribingJson?)
+        Assert.assertEquals("{\"schema\":\"org.test.scheme\",\"data\":{}}", json.toString())
+    }
+
+    @Test
+    fun testGetByteSize() {
+        val payload = TrackerPayload()
+        testMap!!["a"] = "b"
+        payload.addMap(testMap)
+        val json = SelfDescribingJson(testSchema)
+        json.setData(payload)
+        Assert.assertEquals(45, json.byteSize)
+    }
+}
---
+++ snowplow-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/payload/TrackerPayloadTest.kt
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2015-2022 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.payload
+
+import android.util.Base64
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.snowplowanalytics.core.utils.Util.objectMapToString
+import org.json.JSONException
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.UnsupportedEncodingException
+
+@RunWith(AndroidJUnit4::class)
+class TrackerPayloadTest {
+    var payload: TrackerPayload? = null
+    @Before
+    fun setUp() {
+        payload = TrackerPayload()
+    }
+
+    private fun createTestMap(): HashMap<String, Any?> {
+        val map = HashMap<String, Any?>()
+        map["a"] = "string"
+        map["b"] = ""
+        map["c"] = null
+        return map
+    }
+
+    @Test
+    fun testAddKeyWhenValue() {
+        payload!!.add("a", "string")
+        Assert.assertEquals("string", payload!!.map["a"])
+        payload!!.add("a", 123)
+        Assert.assertEquals(123, payload!!.map["a"])
+    }
+
+    @Test
+    fun testNotAddKeyWhenNullOrEmptyValue() {
+        payload!!.add("a", null)
+        Assert.assertFalse(payload!!.map.containsKey("a"))
+        payload!!.add("a", "")
+        Assert.assertFalse(payload!!.map.containsKey("a"))
+    }
+
+    @Test
+    fun testRemoveKeyWhenNullOrEmptyValue() {
+        payload!!.add("a", "string")
+        payload!!.add("a", "")
+        Assert.assertFalse(payload!!.map.containsKey("a"))
+        payload!!.add("a", 123)
+        payload!!.add("a", null)
+        Assert.assertFalse(payload!!.map.containsKey("a"))
+    }
+
+    @Test
+    fun testAddMapWithoutNullValueEntries() {
+        val testMap: Map<String, Any?> = HashMap(createTestMap())
+        payload!!.addMap(testMap)
+        Assert.assertEquals("string", payload!!.map["a"])
+        Assert.assertEquals("", payload!!.map["b"])
+        Assert.assertFalse(payload!!.map.containsKey("c"))
+    }
+
+    @Test
+    fun testDontCrashWhenAddNullMap() {
+        payload!!.addMap(null)
+        Assert.assertTrue(payload!!.map.isEmpty())
+        payload!!.addMap(null, true, "a", "b")
+        Assert.assertTrue(payload!!.map.isEmpty())
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testAddSimpleMapBase64NoEncode() {
+        payload!!.addMap(createTestMap(), false, "enc", "no_enc")
+        val map: Map<String, Any> = payload!!.map
+        Assert.assertFalse(map.containsKey("enc"))
+        Assert.assertTrue(map.containsKey("no_enc"))
+        val json = JSONObject(map["no_enc"] as String)
+        Assert.assertEquals("string", json.getString("a"))
+        Assert.assertEquals("", json.getString("b"))
+        Assert.assertEquals(JSONObject.NULL, json["c"])
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testAddMapBase64Encoded() {
+        payload!!.addMap(createTestMap(), true, "enc", "no_enc")
+        val map = objectMapToString(
+            payload!!.map
+        )
+        Assert.assertFalse(map.containsKey("no_enc"))
+        val base64Json = payload!!.map["enc"] as String?
+        var jsonString = ""
+        try {
+            jsonString = String(Base64.decode(base64Json, Base64.DEFAULT), Charsets.UTF_8)
+        } catch (e: UnsupportedEncodingException) {
+            e.printStackTrace()
+            Assert.fail("UnsupportedEncodingException")
+        }
+        val json = JSONObject(jsonString)
+        Assert.assertEquals("string", json.getString("a"))
+        Assert.assertEquals("", json.getString("b"))
+        Assert.assertEquals(JSONObject.NULL, json["c"])
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testSimplePayloadToString() {
+        payload!!.add("a", "string")
+        val map = JSONObject(payload.toString())
+        Assert.assertEquals("string", map.getString("a"))
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun testComplexPayloadToString() {
+        payload!!.add("a", createTestMap())
+        val map = JSONObject(payload.toString())
+        val innerMap = map.getJSONObject("a")
+        Assert.assertEquals("string", innerMap.getString("a"))
+    }
+}
