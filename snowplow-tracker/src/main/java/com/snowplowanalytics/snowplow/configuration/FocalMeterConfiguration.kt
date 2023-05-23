2f01fbba724584b1af14a8e12575bd158f8ec6e9
Matúš Tomlein <matus.tomlein@gmail.com>
2023-05-23 10:10:04 +0200

    Add configuration to send requests with user ID to a Focal Meter endpoint (close #571)

---
+++ snowplow-tracker/src/main/java/com/snowplowanalytics/snowplow/configuration/FocalMeterConfiguration.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2015-2023 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+package com.snowplowanalytics.snowplow.configuration
+
+import android.net.Uri
+import com.snowplowanalytics.core.tracker.Logger
+import com.snowplowanalytics.snowplow.entity.ClientSessionEntity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+/**
+ * This configuration tells the tracker to send requests with the user ID in session context entity
+ * to a Kantar endpoint used with FocalMeter.
+ * The request is made when the first event with a new user ID is tracked.
+ * The requests are only made if session context is enabled (default).
+ * @param kantarEndpoint The Kantar URI endpoint including the HTTP protocol to send the requests to.
+ */
+class FocalMeterConfiguration(
+    var kantarEndpoint: String
+) : Configuration, PluginConfigurationInterface {
+    private val TAG = FocalMeterConfiguration::class.java.simpleName
+
+    private var lastUserId: String? = null
+
+    override val identifier: String
+        get() = "KantarFocalMeter"
+
+    override val entitiesConfiguration: PluginEntitiesConfiguration?
+        get() = null
+
+    override val afterTrackConfiguration: PluginAfterTrackConfiguration?
+        get() = PluginAfterTrackConfiguration { event ->
+            val session = event.entities.find { it is ClientSessionEntity } as? ClientSessionEntity
+            session?.userId?.let { newUserId ->
+                if (shouldUpdate(newUserId)) {
+                    makeRequest(newUserId)
+                }
+            }
+        }
+
+    /**
+     * The Kantar URI endpoint including the HTTP protocol to send the requests to.
+     */
+    fun kantarEndpoint(kantarEndpoint: String): FocalMeterConfiguration {
+        this.kantarEndpoint = kantarEndpoint
+        return this
+    }
+
+    private fun shouldUpdate(userId: String): Boolean {
+        synchronized(this) {
+            if (lastUserId == null || lastUserId != userId) {
+                lastUserId = userId
+                return true
+            }
+            return false
+        }
+    }
+
+    private fun makeRequest(userId: String) {
+        val uriBuilder = Uri.parse(kantarEndpoint).buildUpon()
+        uriBuilder.appendQueryParameter("vendor", "snowplow")
+        uriBuilder.appendQueryParameter("cs_fpid", userId)
+        uriBuilder.appendQueryParameter("c12", "not_set")
+
+        val client = OkHttpClient.Builder()
+            .connectTimeout(15, java.util.concurrent.TimeUnit.SECONDS)
+            .readTimeout(15, java.util.concurrent.TimeUnit.SECONDS)
+            .build()
+
+        val request = Request.Builder()
+            .url(uriBuilder.build().toString())
+            .build()
+
+        try {
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                Logger.d(TAG, "Request to Kantar endpoint sent with user ID: $userId")
+            } else {
+                Logger.e(TAG, "Request to Kantar endpoint failed with code: ${response.code}")
+            }
+        } catch (e: IOException) {
+            Logger.e(TAG, "Request to Kantar endpoint failed with exception: ${e.message}")
+        }
+    }
+
+    override fun copy(): Configuration {
+        return FocalMeterConfiguration(kantarEndpoint = kantarEndpoint)
+    }
+
+}
